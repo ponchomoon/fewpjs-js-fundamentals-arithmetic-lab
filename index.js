05dbd0ace4fc0fd24191939a22155bfe7ce941ae
ponchomoon <vilmajohannavelasquez@gmail.com>
2021-12-16 18:29:21 +1100

    Completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+//1
+const num1 = 1;
+const num2 = 62;
+//2
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 1000) + 1;
+//3
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+//4
+const max = Math.max(5, 10, 15, 20);
